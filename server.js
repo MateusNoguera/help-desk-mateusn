1663b5fdb81f915a87d3bffde875d3f2e187f031
Mateus Nogueira <mnogueira030@gmail.com>
2026-09-16 16:06:27 -0300

    Added ticket creation and listing endpoints.

--- server.js
+++ server.js
@@ -2,11 +2,45 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
+
+const tickets = [];
 
 app.get("/", (req, res) => {
     res.json({
         message: "API running :)"
+    });
+});
+
+app.get("/tickets", (req, res) => {
+    return res.json(tickets);
+});
+
+app.post("/tickets", (req, res) => {
+    const { title, description, priority } = req.body;
+
+    if (!title || !description) {
+        return res.status(400).json({
+            message: "Title and description are required!"
+        });
+    }
+
+    const ticket = {
+        id: tickets.length + 1,
+        title: title,
+        description: description,
+        priority: priority || "MEDIUM",
+        status: "OPEN",
+        createdAt: new Date()
+    };
+
+    tickets.push(ticket);
+
+    return res.status(201).json({
+        message: "Ticket created",
+        ticket: ticket
     });
 });
 
