8ab9deba38e01675013f6600da954f4e9887b6c7
Mateus Nogueira <mnogueira030@gmail.com>
2026-09-24 20:37:52 -0300

    Refactored tickets route into controller.

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,153 +1,16 @@
 import express from "express";
-import pool from "../database/db.js";
+import { createTicket, deleteTicket, getTickets, getTicketsById, updateTicketStatus } from "../controllers/ticketsController.js";
 
 const router = express.Router();
 
-const validStatuses = [
-    "OPEN",
-    "IN_PROGRESS",
-    "RESOLVED",
-    "CLOSED"
-];
+router.get("/", getTickets);
 
-const validPriorities = [
-    "LOW",
-    "MEDIUM",
-    "HIGH",
-    "CRITICAL"
-];
+router.get("/:id", getTicketsById);
 
-router.get("/", async (req, res) => {
-    try {
-        const result = await pool.query(
-            "SELECT * FROM tickets ORDER BY id"
-        );
+router.post("/", createTicket);
 
-        return res.json(result.rows);    
-    } catch (error) {
-        console.error(error);
+router.patch("/:id", updateTicketStatus);
 
-        return res.status(500).json({
-            message: "Internal server error"
-        });
-    }
-});
-
-router.get("/:id", async (req, res) => {
-    try {
-        const id = Number(req.params.id);
-
-        const result = await pool.query(
-            "SELECT * FROM tickets WHERE id = $1",
-            [id]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                message: "Ticket not found!"
-            });
-        }
-
-        return res.json(result.rows[0]);
-    } catch (error) {
-        console.error(error);
-
-        return res.status(500).json({
-            message: "Internal server error"
-        });
-    }
-});
-
-router.post("/", async (req, res) => {
-    try {
-
-        const { title, description, priority } = req.body;
-
-        if (!title || !description) {
-            return res.status(400).json({
-                message: "Title and description are required!"
-            });
-        }
-
-        if (priority && !validPriorities.includes(priority)) {
-            return res.status(400).json({
-                message: "Invalid priority!"
-            });
-        }
-
-        const result = await pool.query(
-            `
-                INSERT INTO tickets (title, description, priority)
-                VALUES ($1, $2, $3)
-                RETURNING *
-            `,
-            [
-                title,
-                description,
-                priority || "MEDIUM"
-            ]
-        );
-
-        return res.status(201).json({
-            message: "Ticket created!",
-            ticket: result.rows[0]
-        });
-
-    } catch (error) {
-        
-        console.error(error);
-
-        return res.status(500).json({
-            message: "Internal server error!"
-        });
-
-    }
-});
-
-router.patch("/:id", async (req, res) => {
-    try {
-        const id = Number(req.params.id);
-        const { status } = req.body;
-
-        if (!status) {
-            return res.status(400).json({
-                message: "Status is required!"
-            });
-        }
-
-        if (!validStatuses.includes(status)) {
-            return res.status(400).json({
-                message: "Invalid status!"
-            });
-        }
-
-        const result = await pool.query(
-            `
-                UPDATE tickets
-                SET status = $1
-                WHERE id = $2
-                RETURNING *
-            `,
-            [status, id]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({
-                message: "Ticket not found!"
-            });
-        }
-
-        return res.json({
-            message: "Ticket updated!",
-            ticket: result.rows[0]
-        });    
-    } catch (error) {
-        console.error(error);
-
-        return res.status(500).json({
-            message: "Internal server error!"
-        });
-    }
-});
+router.delete("/:id", deleteTicket);
 
 export default router;
